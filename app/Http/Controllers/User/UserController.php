5ec950f75ecdd9229b10e729a05aef46dd3b2449
1735455665liu <1735455665@qq.com>
2019-04-10 22:07:46 +0800

    修改

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\DB;
+class UserController extends Controller
+{
+
+    /**
+     * 接收微信事件推送 POST
+     */
+
+}
