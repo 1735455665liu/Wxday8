dc50cf095d2526b5cfbc19a474df191d011d989c
1735455665@qq.com <you@173455665@qq.com>
2019-04-11 15:10:50 +0000

    修改测试

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//微信接口接入
-Route::get('valid','User\UserController@valid');
+
+
+
+Route::get('/test/redis/aaa','Test\TestController@aaa');
+
+Route::get('/test/atoken','User\UserController@atoken');
+//微信首次接入
+Route::get('/weixin/valid','User\UserController@valid');
 //接收微信服务器推送事件
 Route::post('/weixin/valid','User\UserController@wxEvent');
-
-
+Route::get('/weixin/create_menu','User\UserController@createMenu');     //创建公众号菜单
 Route::get('/weixin/get_access_token','User\UserController@getAccessToken');
 Route::get('/weixin/test','User\UserController@test');
