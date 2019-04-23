8b0d31b023caeebb8f14233f18eec82eea51fd46
1735455665liu <1735455665@qq.com>
2019-04-23 17:41:01 +0800

    签名加密

--- app/Http/Controllers/jssdk/jssdkController.php
+++ app/Http/Controllers/jssdk/jssdkController.php
@@ -16,12 +16,18 @@ class jssdkController extends Controller
         $timestamp=time();  //当前时间
         $nonceStr=Str::random(10);//随机字符串
         $url=$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+
+
+        //计算拼接签名
+        $strsha="jsapi_ticket=$ticket&timestamp=$timestamp&nonceStr=$nonceStr&signature=$url";
+        //加密签名
+        $sign=sha1($strsha);
         $js_jssdk=[
             "appId"=>env('WX_APP_ID'),//公总号id
             "timestamp"=>$timestamp,//生成签名时间戳
             "nonceStr"=>$nonceStr,//生成签名的随机字符串
-            "signature"=>$ticket,//签名
-            "jsApiList"=>$url// 必填，需要使用的JS接口列表
+            "signature"=>$sign,//签名
         ];
         $data=[
             'js_jssdk'=>$js_jssdk
